f2adf8902e5a8f4342b4cb58280d18063fae60b8
Column01 <Column01Dev@gmail.com>
2024-01-15 14:17:48 -0500

    Added travel metrics

--- README.md
+++ README.md
@@ -15,20 +15,18 @@ Python code that takes a neutron plotter route and automatically copies the next
 ### General
 
 - [ ] Test with an actual route
-- [ ] Automatically gather the latest journal (or maybe just use a file picker again...)
 - [x] Update install and usage guides
 - Travel metrics
-  - [ ] Distance Traveled
-  - [ ] Jumps remaining out of (estimated) Total
-  - [ ] Journey time length? (save timestamp of when the first jump is made and calculate trip time when last jump is complete)
+  - [x] Distance Traveled
+  - [x] Jumps made
 
 ### UI Related
 
 - [x] Make a UI
-- [ ] Display current System info
-- [ ] Display nav target system info
-- [ ] Display in game target system info
+- [x] Display current System info
+- [x] Display nav target system info
+- [x] Display in game target system info
   - Provided in journals as `FSDTarget`
-- [ ] Display remaining/total jumps in route
+- [x] Display remaining/total jumps in route
   - Info will be from spansh route and may not be accurate as a result
   - Maybe also display actual jumps remaining for current nav target from the journal (provided in `FSDTarget` again)
--- main.py
+++ main.py
@@ -19,11 +19,15 @@ class UI:
         self.master.title("ED Route Copier")
         self.master.geometry("800x400")
         self.master.minsize(*MINSIZE)
+
+        # Filled when Load route button is clicked
         self.journal_file_path = None
         self.route_file_path = None
         self.last_jump = False
-
-        self.route_copier = None # Initialized when the "Load Route" button is clicked. See self.load_route
+        self.total_distance = 0
+        self.total_jumps = 0
+        self.estimated_jumps = 0
+        self.route_copier = None
 
     def create_widgets(self) -> None:
         journal_frame = tk.Frame(self.master)
@@ -59,17 +63,25 @@ class UI:
 
         self.target_system_label = ttk.Label(status_frame, text="Target System: None")
         self.target_system_label.pack(side=tk.LEFT, **PAD)
-        
-        self.jumps_remaining_label = ttk.Label(status_frame, text="Jumps Remaining: 0")
-        self.jumps_remaining_label.pack(side=tk.LEFT, **PAD)
 
         self.route_target_label = ttk.Label(status_frame, text="Next Route Target: None")
         self.route_target_label.pack(side=tk.LEFT, **PAD)
 
+        metrics_frame = tk.Frame(self.master)
+        metrics_frame.pack(fill=tk.X)
+        self.jumps_remaining_label = ttk.Label(metrics_frame, text="Jumps Remaining: 0")
+        self.jumps_remaining_label.pack(side=tk.LEFT, **PAD)
+
+        self.total_jumps_label = ttk.Label(metrics_frame, text="Jumps Made/Total: 0/Unknown")
+        self.total_jumps_label.pack(side=tk.LEFT, **PAD)
+
+        self.dist_traveled_label = ttk.Label(metrics_frame, text="Distance Traveled: 0ly")
+        self.dist_traveled_label.pack(side=tk.LEFT, **PAD)
+
         text_frame = tk.Frame(self.master)
         text_frame.pack(fill=tk.BOTH, expand=True, **PAD)
         # Text box for info from the route copier code
-        self.info_box = scrolledtext.ScrolledText(text_frame, height=14, state="disabled", **PAD)
+        self.info_box = scrolledtext.ScrolledText(text_frame, height=13, state="disabled", **PAD)
         self.info_box.pack(side=tk.TOP, fill=tk.BOTH)
 
         button_frame = tk.Frame(self.master)
@@ -78,8 +90,9 @@ class UI:
         self.button = ttk.Button(button_frame, text="Load Route", command=self.load_route)
         self.button.pack(side=tk.LEFT, **PAD)
 
-    def _nav_generator(self) -> dict:
-        self.write_text(f"Loading neutron route from file: {self.route_file_path}")
+    def _nav_generator(self, verbose=True) -> dict:
+        if verbose:
+            self.write_text(f"Loading neutron route from file: {self.route_file_path}")
         with open(self.route_file_path, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
@@ -127,12 +140,25 @@ class UI:
             if os.path.isdir(path):
                 return path
         return "/"
+    
+    def get_estimated_jumps(self) -> int:
+        return sum(int(target["Jumps"]) for target in self._nav_generator(verbose=False))
 
     def on_journal(self, json_data: dict) -> None:
         event = json_data.get("event")
         # CMDR has FSD Jumped to a system
         if event == "FSDJump":
+            # Get the system we entered
             star_system = json_data.get("StarSystem")
+
+            # Metrics
+            distance_jumped = json_data.get("JumpDist")
+            self.total_distance += distance_jumped
+            self.total_jumps += 1
+            self.update_label(self.dist_traveled_label, f"Distance Traveled: {self.total_distance:.2f}ly")
+            self.update_label(self.total_jumps_label, f"Jumps Made/Total: {self.total_jumps}/{self.estimated_jumps}")
+
+            # Log the jump
             self.write_text(f"You have arrived in {star_system}")
             self.update_label(self.cur_system_label, f"Current System: {star_system}")
             # Check if the system is the one we were targetting, if it is, copy the next system to the clipboard
@@ -167,7 +193,7 @@ class UI:
         elif event == "Shutdown":
             # Player shut the game down
             self.update_label(self.cur_system_label, "Current System: Unknown")
-            self.update_label(self.cur_system_label, "Target System: None")
+            self.update_label(self.route_target_label, "Next Route Target: None")
             self.update_label(self.jumps_remaining_label, f"Jumps Remaining: 0")
             self.update_label(self.target_system_label, f"Target System: None")
             self.write_text("Game has been shut down")
@@ -176,16 +202,27 @@ class UI:
             # Game has started, load some info
             star_system = json_data.get("StarSystem")
             self.update_label(self.cur_system_label, f"Current System: {star_system}")
+        
+    def reset_metrics(self) -> None:
+        self.total_distance = 0
+        self.total_jumps = 0
+        self.estimated_jumps = 0
 
     def load_route(self) -> None:
         if self.journal_file_path:
             if self.route_file_path:
+                # Initialize the log watcher and listen for all journal events
                 self.log_watcher = EDLogWatcher(self.journal_file_path, self.route_file_path)
                 start = self.log_watcher.start()
                 self.log_watcher.register_callback("all", self.on_journal)
                 # Turn the neutron route into a generator object and get the first target
                 self.nav = self._nav_generator()
                 self.nav_target = next(self.nav)
+                # Reset the metrics, gather the estimated jumps required and update the label
+                self.reset_metrics()
+                self.estimated_jumps = self.get_estimated_jumps()
+                self.update_label(self.total_jumps_label, f"Jumps Made/Total: {self.total_jumps}/{self.estimated_jumps}")
+                # Clear the info box so we know whats going on
                 self.write_text("", clear=True)
                 if not start:
                     messagebox.showerror("Error when starting the route copier! Please report any errors in your console to the github")
